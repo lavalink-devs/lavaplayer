52d6268acc1d460cb32e0efdd5576413f5eb6137
Walkyst <41806921+Walkyst@users.noreply.github.com>
2021-06-18 20:46:13 +0300

    Switch YT mix to loading from innertube

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeMixProvider.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/YoutubeMixProvider.java
@@ -1,23 +1,26 @@
 package com.sedmelluq.discord.lavaplayer.source.youtube;
 
+import com.sedmelluq.discord.lavaplayer.tools.DataFormatTools;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.tools.JsonBrowser;
 import com.sedmelluq.discord.lavaplayer.tools.PBJUtils;
-import com.sedmelluq.discord.lavaplayer.tools.Units;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import com.sedmelluq.discord.lavaplayer.track.BasicAudioPlaylist;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 
+import static com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeConstants.NEXT_PAYLOAD;
+import static com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeConstants.NEXT_URL;
 import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.SUSPICIOUS;
 
 /**
@@ -40,15 +43,15 @@ public class YoutubeMixProvider implements YoutubeMixLoader {
     String playlistTitle = "YouTube mix";
     List<AudioTrack> tracks = new ArrayList<>();
 
-    String mixUrl = "https://www.youtube.com/watch?v=" + selectedVideoId + "&list=" + mixId + "&pbj=1";
-
-    try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(mixUrl))) {
+    HttpPost post = new HttpPost(NEXT_URL);
+    StringEntity payload = new StringEntity(String.format(NEXT_PAYLOAD, selectedVideoId, mixId), "UTF-8");
+    post.setEntity(payload);
+    try (CloseableHttpResponse response = httpInterface.execute(post)) {
       HttpClientTools.assertSuccessWithContent(response, "mix response");
 
       JsonBrowser body = JsonBrowser.parse(response.getEntity().getContent());
-      JsonBrowser playlist = body.index(3).get("response")
-              .get("contents")
-              .get("twoColumnWatchNextResults")
+      JsonBrowser playlist = body.get("contents")
+              .get("singleColumnWatchNextResults")
               .get("playlist")
               .get("playlist");
 
@@ -78,33 +81,16 @@ public class YoutubeMixProvider implements YoutubeMixLoader {
   ) {
     for (JsonBrowser video : browser.values()) {
       JsonBrowser renderer = video.get("playlistPanelVideoRenderer");
-      String title = renderer.get("title").get("simpleText").text();
+      String title = renderer.get("title").get("runs").index(0).get("text").text();
       String author = renderer.get("longBylineText").get("runs").index(0).get("text").text();
-      String durationStr = renderer.get("lengthText").get("simpleText").text();
-      long duration = parseDuration(durationStr);
+      String durationStr = renderer.get("lengthText").get("runs").index(0).get("text").text();
+      long duration = DataFormatTools.durationTextToMillis(durationStr);
       String identifier = renderer.get("videoId").text();
       String uri = "https://youtube.com/watch?v=" + identifier;
 
       AudioTrackInfo trackInfo = new AudioTrackInfo(title, author, duration, identifier, false, uri,
               PBJUtils.getYouTubeThumbnail(identifier));
       tracks.add(trackFactory.apply(trackInfo));
-    }
-  }
-
-  private long parseDuration(String duration) {
-    String[] parts = duration.split(":");
-
-    if (parts.length == 3) { // hh::mm:ss
-      int hours = Integer.parseInt(parts[0]);
-      int minutes = Integer.parseInt(parts[1]);
-      int seconds = Integer.parseInt(parts[2]);
-      return (hours * 3600000) + (minutes * 60000) + (seconds * 1000);
-    } else if (parts.length == 2) { // mm:ss
-      int minutes = Integer.parseInt(parts[0]);
-      int seconds = Integer.parseInt(parts[1]);
-      return (minutes * 60000) + (seconds * 1000);
-    } else {
-      return Units.DURATION_MS_UNKNOWN;
     }
   }
 
