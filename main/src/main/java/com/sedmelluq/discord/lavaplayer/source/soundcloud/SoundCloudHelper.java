76f57f36d4b8ecc2e293834be5335cbe724a685f
Walkyst <41806921+Walkyst@users.noreply.github.com>
2021-09-09 21:52:50 +0300

    Support for private playlists and mobile links

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/SoundCloudHelper.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/SoundCloudHelper.java
@@ -1,11 +1,21 @@
 package com.sedmelluq.discord.lavaplayer.source.soundcloud;
 
+import com.sedmelluq.discord.lavaplayer.tools.ExceptionTools;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.tools.JsonBrowser;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
 import com.sedmelluq.discord.lavaplayer.tools.io.PersistentHttpStream;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
+
+import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.SUSPICIOUS;
 
 public class SoundCloudHelper {
   public static String nonMobileUrl(String url) {
@@ -24,6 +34,22 @@ public class SoundCloudHelper {
 
       JsonBrowser json = JsonBrowser.parse(stream);
       return json.get("url").text();
+    }
+  }
+
+  public static AudioReference redirectMobileLink(HttpInterface httpInterface, AudioReference reference) {
+    try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(reference.identifier))) {
+      HttpClientTools.assertSuccessWithContent(response, "mobile redirect response");
+      HttpClientContext context = httpInterface.getContext();
+      List<URI> redirects = context.getRedirectLocations();
+      if (redirects != null && !redirects.isEmpty()) {
+        return new AudioReference(redirects.get(0).toString(), null);
+      } else {
+        throw new FriendlyException("Unable to process soundcloud mobile link", SUSPICIOUS,
+            new IllegalStateException("Expected soundcloud to redirect soundcloud.app.goo.gl link to a valid track/playlist link, but it did not redirect at all"));
+      }
+    } catch (Exception e) {
+      throw ExceptionTools.wrapUnfriendlyExceptions(e);
     }
   }
 }
