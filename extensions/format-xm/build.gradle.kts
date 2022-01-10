772136c3c5ebe7cc6c31eb9b726e0fad97125ad3
Walkyst <41806921+Walkyst@users.noreply.github.com>
2022-01-10 16:03:02 +0300

    Actually fix resolving of IBXM dependency

--- extensions/format-xm/build.gradle.kts
+++ extensions/format-xm/build.gradle.kts
@@ -5,19 +5,9 @@ plugins {
 val moduleName = "lavaplayer-ext-format-xm"
 version = "0.1.0"
 
-repositories {
-  ivy {
-    url = uri("https://github.com/")
-    patternLayout {
-      artifact("martincameron/micromod/raw/master/[module]-[revision].[ext]")
-    }
-    metadataSources { artifact() }
-  }
-}
-
 dependencies {
   compileOnly(project(":main"))
-  implementation("micromod:ibxm:a74:jar")
+  implementation("com.github.walkyst:ibxm-fork:a75")
   implementation("org.slf4j:slf4j-api:1.7.25")
 }
 
@@ -26,24 +16,12 @@ val sourcesJar by tasks.registering(Jar::class) {
   from(sourceSets["main"].allSource)
 }
 
-val uberJar by tasks.registering(Jar::class) {
-  archiveClassifier.set("uber")
-
-  from(sourceSets.main.get().output)
-
-  dependsOn(configurations.runtimeClasspath)
-  from({
-    configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
-  })
-}
-
 publishing {
   publications {
     create<MavenPublication>("mavenJava") {
       from(components["java"])
       artifactId = moduleName
       artifact(sourcesJar)
-      artifact(uberJar)
     }
   }
 }
