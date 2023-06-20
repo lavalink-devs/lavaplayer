c4b279e83a0b68adf9daeada5498364855c4baec
Freya Arbjerg <freya@arbjerg.dev>
2023-06-20 14:24:21 +0200

    Add support for Soundcloud short URLs

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/SoundCloudHelper.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/soundcloud/SoundCloudHelper.java
@@ -1,5 +1,6 @@
 package com.sedmelluq.discord.lavaplayer.source.soundcloud;
 
+import com.sedmelluq.discord.lavaplayer.tools.DataFormatTools;
 import com.sedmelluq.discord.lavaplayer.tools.ExceptionTools;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.tools.JsonBrowser;
@@ -7,12 +8,14 @@ import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
 import com.sedmelluq.discord.lavaplayer.tools.io.PersistentHttpStream;
 import com.sedmelluq.discord.lavaplayer.track.AudioReference;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
 
 import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 import static com.sedmelluq.discord.lavaplayer.tools.FriendlyException.Severity.SUSPICIOUS;
@@ -48,6 +51,21 @@ public class SoundCloudHelper {
         throw new FriendlyException("Unable to process soundcloud mobile link", SUSPICIOUS,
             new IllegalStateException("Expected soundcloud to redirect soundcloud.app.goo.gl link to a valid track/playlist link, but it did not redirect at all"));
       }
+    } catch (Exception e) {
+      throw ExceptionTools.wrapUnfriendlyExceptions(e);
+    }
+  }
+
+  public static AudioReference resolveShortTrackUrl(HttpInterface httpInterface, AudioReference reference) {
+    try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(reference.identifier))) {
+      HttpClientTools.assertSuccessWithContent(response, "short url resolution");
+      String responseText = IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+      String url = DataFormatTools.extractBetween(responseText, "<link rel=\"canonical\" href=\"", "\">");
+      if (url == null) {
+        throw new FriendlyException("Unable to resolve Soundcloud short URL", SUSPICIOUS,
+            new IllegalStateException("Unable to locate canonical URL"));
+      }
+      return new AudioReference(url, null);
     } catch (Exception e) {
       throw ExceptionTools.wrapUnfriendlyExceptions(e);
     }
