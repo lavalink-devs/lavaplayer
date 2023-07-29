d5a6ea710eadf1cf6a6ff705ca33b46d1c267dbd
Toπ <git@topi.wtf>
2023-07-29 13:08:42 +0200

    update gradle wrapper & fix versioning
    # Conflicts:
    #	build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,17 @@
-import com.palantir.gradle.gitversion.VersionDetails
-import groovy.lang.Closure
-import java.net.URI
+import org.ajoberstar.grgit.Grgit
 
 plugins {
   java
   `maven-publish`
-  id("com.palantir.git-version") version "3.0.0"
+  id("org.ajoberstar.grgit") version "5.2.0"
 }
 
-group = "dev.arbjerg"
-val gitVersion: Closure<String> by extra
-val versionDetails: Closure<VersionDetails> by extra
-val details = versionDetails()
-version = gitVersion()
-println("Version: $version")
-println("Details: $details")
+val (gitVersion, release) = versionFromGit()
+println("Version: $gitVersion (release: $release)")
 
 allprojects {
-  group = rootProject.group
-  version = rootProject.version
+  group = "dev.arbjerg"
+  version = gitVersion
 
   repositories {
     mavenLocal()
@@ -26,10 +19,11 @@ allprojects {
     maven(url = "https://m2.dv8tion.net/releases")
     maven(url = "https://jitpack.io")
   }
+}
 
+subprojects {
   apply(plugin = "java")
   apply(plugin = "maven-publish")
-  apply(plugin = "com.palantir.git-version")
 
   java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -41,12 +35,28 @@ allprojects {
       val snapshots = "https://maven.arbjerg.dev/snapshots"
       val releases = "https://maven.arbjerg.dev/releases"
 
-      maven(if (details.isCleanTag) releases else snapshots) {
+      maven(if (release) releases else snapshots) {
         credentials {
           password = findProperty("MAVEN_PASSWORD") as String?
           username = findProperty("MAVEN_USERNAME") as String?
         }
       }
     }
+  }
+}
+
+@SuppressWarnings("GrMethodMayBeStatic")
+fun versionFromGit(): Pair<String, Boolean> {
+  Grgit.open(mapOf("currentDir" to project.rootDir)).use { git ->
+    val headTag = git.tag
+      .list()
+      .find { it.commit.id == git.head().id }
+
+    val clean = git.status().isClean || System.getenv("CI") != null
+    if (!clean) {
+      println("Git state is dirty, version is a snapshot.")
+    }
+
+    return if (headTag != null && clean) Pair(headTag.name, true) else Pair(git.head().abbreviatedId, false)
   }
 }
