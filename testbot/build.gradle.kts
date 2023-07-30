a9fede5dd6c5abcd42c26cea66f47177e1496561
mik <michael@homedia.com>
2023-07-30 23:47:19 +0200

    Final touches to build logic

--- testbot/build.gradle.kts
+++ testbot/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 }
 
 dependencies {
-  implementation(project(":main"))
+  implementation(projects.main)
   implementation("net.dv8tion:JDA:4.2.1_253")
   implementation("net.iharder:base64:2.3.9")
   runtimeOnly("ch.qos.logback:logback-classic:1.1.8")
 }
 
 application {
-  mainClass.set("com.sedmelluq.discord.lavaplayer.demo.Bootstrap")
+  mainClass = "com.sedmelluq.discord.lavaplayer.demo.Bootstrap"
 }
