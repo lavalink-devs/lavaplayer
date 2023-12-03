9456b26c05cf4cc920e80f723cd60467c21b47bb
Toπ <git@topi.wtf>
2023-12-03 02:35:12 +0100

    update instructions

--- natives/instructions/linux-glibc-x86-64.sh
+++ natives/instructions/linux-glibc-x86-64.sh
@@ -1,25 +1,20 @@
 docker run --rm -it ubuntu:18.04 bash
 
 apt update && apt upgrade
-apt install -y git cmake build-essential autoconf openjdk-17-jdk wget nano
+apt install -y git cmake build-essential autoconf openjdk-17-jdk wget nano libsamplerate-dev
 
 wget http://ftp.de.debian.org/debian/pool/main/a/automake-1.16/automake_1.16.1-4_all.deb
 dpkg -i automake_1.16.1-4_all.deb
 
-git clone https://github.com/markozajc/lavaplayer
+git clone https://github.com/lavalink-devs/lavaplayer
+git checkout patch/bump-natives
 cd lavaplayer
 chmod +x ./gradlew
 cd natives
 
-nano build.gradle >> ext.mpg123Version = '1.32.3'
-
 ../gradlew load
 
 # comment out PKG_CHECK_MODULES: L
-
-# downgrade cmake version to 3.10
-
-apt install libsamplerate-dev
 
 touch opus/opus-1.4/aclocal.m4 configure
 ../gradlew compileNatives
