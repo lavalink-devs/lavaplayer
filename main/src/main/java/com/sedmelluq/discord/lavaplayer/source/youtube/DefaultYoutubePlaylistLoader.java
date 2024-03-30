c8a8d821ea5ae92d8e618d67c1a26bf22a9c1e16
Kamilake <exjang0@gmail.com>
2024-03-30 10:43:39 +0900

    Change default player to WEB

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/DefaultYoutubePlaylistLoader.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/youtube/DefaultYoutubePlaylistLoader.java
@@ -36,7 +36,7 @@ public class DefaultYoutubePlaylistLoader implements YoutubePlaylistLoader {
     public AudioPlaylist load(HttpInterface httpInterface, String playlistId, String selectedVideoId,
                               Function<AudioTrackInfo, AudioTrack> trackFactory) {
         HttpPost post = new HttpPost(BROWSE_URL);
-        YoutubeClientConfig clientConfig = YoutubeClientConfig.ANDROID.copy()
+        YoutubeClientConfig clientConfig = YoutubeClientConfig.WEB.copy()
             .withRootField("browseId", "VL" + playlistId)
             .setAttribute(httpInterface);
         StringEntity payload = new StringEntity(clientConfig.toJsonString(), "UTF-8");
@@ -90,7 +90,7 @@ public class DefaultYoutubePlaylistLoader implements YoutubePlaylistLoader {
         // Also load the next pages, each result gives us a JSON with separate values for list html and next page loader html
         while (continuationsToken != null && ++loadCount < pageCount) {
             HttpPost post = new HttpPost(BROWSE_URL);
-            YoutubeClientConfig clientConfig = YoutubeClientConfig.ANDROID.copy()
+            YoutubeClientConfig clientConfig = YoutubeClientConfig.WEB.copy()
                 .withRootField("continuation", continuationsToken)
                 .setAttribute(httpInterface);
             StringEntity payload = new StringEntity(clientConfig.toJsonString(), "UTF-8");
